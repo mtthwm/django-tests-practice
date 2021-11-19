de371c0221ff359708005bb4e7c07493f6bad917
Matthew <mtthwmrls9@gmail.com>
2021-11-19 12:28:26 -0700

    Modifying the relationship between blog posts and post tags

--- homepage/models.py
+++ homepage/models.py
@@ -15,12 +15,17 @@ class BlogPostManager(models.Manager):
     def published (self):
         return self.get_queryset().filter(published=True)
 
+class PostTag (models.Model):
+    title = models.CharField(max_length=50, null=False)
+    color = ColorField(default="#0099cc")
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=250, null=False)
     title_slug = models.SlugField(primary_key=True, unique=True, max_length=50)
     content = RichTextUploadingField()
     published = models.BooleanField(default=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(PostTag)
 
     objects = BlogPostManager()
 
@@ -33,8 +38,4 @@ class PortfolioProject(models.Model):
     description = RichTextUploadingField()
     ongoing = models.BooleanField(default=False)
     main_image = models.ImageField(upload_to='public/portfolioimages')
-
-class PostTag (models.Model):
-    post_ref = models.ForeignKey(BlogPost, on_delete=models.DO_NOTHING, null=False)
-    title = models.CharField(max_length=50, null=False)
-    color = ColorField(default="#0099cc")
+    associated_posts = models.ManyToManyField(BlogPost)
